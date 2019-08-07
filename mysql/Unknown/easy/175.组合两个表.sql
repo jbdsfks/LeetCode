d3860401a28e0ea4ead6f94de042e755fcc71ef3
jbdsfks <838279926@qq.com>
2019-08-07 10:48:22 +0800

    更新目录结构至${language}/${tag}/${difficulty}

---
+++ mysql/Unknown/easy/175.组合两个表.sql
@@ -0,0 +1,54 @@
+--
+-- @lc app=leetcode.cn id=175 lang=mysql
+--
+-- [175] 组合两个表
+--
+-- https://leetcode-cn.com/problems/combine-two-tables/description/
+--
+-- database
+-- Easy (69.43%)
+-- Likes:    414
+-- Dislikes: 0
+-- Total Accepted:    58.2K
+-- Total Submissions: 83.7K
+-- Testcase Example:  '{"headers": {"Person": ["PersonId", "LastName", "FirstName"], "Address": ["AddressId", "PersonId", "City", "State"]}, "rows": {"Person": [[1, "Wang", "Allen"]], "Address": [[1, 2, "New York City", "New York"]]}}'
+--
+-- 表1: Person
+-- 
+-- +-------------+---------+
+-- | 列名         | 类型     |
+-- +-------------+---------+
+-- | PersonId    | int     |
+-- | FirstName   | varchar |
+-- | LastName    | varchar |
+-- +-------------+---------+
+-- PersonId 是上表主键
+-- 
+-- 
+-- 表2: Address
+-- 
+-- +-------------+---------+
+-- | 列名         | 类型    |
+-- +-------------+---------+
+-- | AddressId   | int     |
+-- | PersonId    | int     |
+-- | City        | varchar |
+-- | State       | varchar |
+-- +-------------+---------+
+-- AddressId 是上表主键
+-- 
+-- 
+-- 
+-- 
+-- 编写一个 SQL 查询，满足条件：无论 person 是否有地址信息，都需要基于上述两表提供 person 的以下信息：
+-- 
+-- 
+-- 
+-- FirstName, LastName, City, State
+-- 
+-- 
+--
+# Write your MySQL query statement below
+SELECT FirstName, LastName, City, State 
+FROM Person LEFT JOIN Address ON Person.PersonId = Address.PersonId;
+
---
+++ mysql/Unknown/easy/176.第二高的薪水.sql
@@ -0,0 +1,39 @@
+--
+-- @lc app=leetcode.cn id=176 lang=mysql
+--
+-- [176] 第二高的薪水
+--
+-- https://leetcode-cn.com/problems/second-highest-salary/description/
+--
+-- database
+-- Easy (31.81%)
+-- Likes:    346
+-- Dislikes: 0
+-- Total Accepted:    41.3K
+-- Total Submissions: 129.9K
+-- Testcase Example:  '{"headers": {"Employee": ["Id", "Salary"]}, "rows": {"Employee": [[1, 100], [2, 200], [3, 300]]}}'
+--
+-- 编写一个 SQL 查询，获取 Employee 表中第二高的薪水（Salary） 。
+-- 
+-- +----+--------+
+-- | Id | Salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- 
+-- 
+-- 例如上述 Employee 表，SQL查询应该返回 200 作为第二高的薪水。如果不存在第二高的薪水，那么查询应返回 null。
+-- 
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | 200                 |
+-- +---------------------+
+-- 
+-- 
+--
+# Write your MySQL query statement below
+
+
